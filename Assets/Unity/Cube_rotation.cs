31c3de8b6bb45a26d84925260732b391e16b7b23
shimismith <smith.shimi@gmail.com>
2018-06-19 21:55:50 -0400

    Cleaned up cobe code

--- Assets/Unity/Cube_rotation.cs
+++ Assets/Unity/Cube_rotation.cs
@@ -4,41 +4,79 @@ using UnityEngine;
 
 public class Cube_rotation : MonoBehaviour{
 	// Use this for initialization
-		TKSwipeRecognizer swipeRecognizer = new TKSwipeRecognizer();
-		public float rotationFactor = 2f;		
+	TKSwipeRecognizer swipeRecognizer = new TKSwipeRecognizer();
+	private float speed = 0;  // degrees per second
+
+	// used to scale the swipe speed to a suitable rotation speed
+	private const float speedScale = 3f;
+
+	// used to slow the rotation down
+	private const float friction = 1;
+
 	void Start () {
 		// var swipeRecognizer = new TKSwipeRecognizer();
 		swipeRecognizer.gestureRecognizedEvent += ( r ) =>
 		{
 			Debug.Log( "swipe recognizer fired: " + r );
+			updateDirection();
 		};
 		TouchKit.addGestureRecognizer( swipeRecognizer );
 	}
-	
-	/* Update is called once per frame
-	 * a good guide on rotations
-	 * https://gamedev.stackexchange.com/questions/136174/im-rotating-an-object-on-two-axes-so-why-does-it-keep-twisting-around-the-thir
-	 */
+
+	// called once per frame
+	/*
+	Called once per frame
+	Here is a good guide on rotations,
+	https://gamedev.stackexchange.com/questions/136174/im-rotating-an-object-on-two-axes-so-why-does-it-keep-twisting-around-the-thir
+	*/
 	void Update () {
-			//detected a swipe
-		if(swipeRecognizer.completedSwipeDirection == TKSwipeDirection.Left){
-			Quaternion targetRotation = Quaternion.AngleAxis(90f,Vector3.up) * this.transform.rotation; // the order of these multiplications matter
-			StartCoroutine(rotateOverTime(this.transform.rotation,targetRotation,rotationFactor));
-			swipeRecognizer.resetSwipeDirection();
-			ConstructCardDesc.saveInstance();
-		} else if(swipeRecognizer.completedSwipeDirection == TKSwipeDirection.Right){
-			Quaternion targetRotation = Quaternion.AngleAxis(90f,Vector3.down) * this.transform.rotation;
-			StartCoroutine(rotateOverTime(this.transform.rotation,targetRotation,rotationFactor));
-			swipeRecognizer.resetSwipeDirection();
-		} else if(swipeRecognizer.completedSwipeDirection == TKSwipeDirection.Up){
-			Quaternion targetRotation = Quaternion.AngleAxis(90f,Vector3.right) * this.transform.rotation;
-			StartCoroutine(rotateOverTime(this.transform.rotation,targetRotation,rotationFactor));
-			swipeRecognizer.resetSwipeDirection();	
-		} else if(swipeRecognizer.completedSwipeDirection == TKSwipeDirection.Down){
-			Quaternion targetRotation = Quaternion.AngleAxis(90f,Vector3.left) * this.transform.rotation;
-			StartCoroutine(rotateOverTime(this.transform.rotation,targetRotation,rotationFactor));
-			swipeRecognizer.resetSwipeDirection();	
+		if (speed <= 0){
+			speed = 0;
 		}
+		else{
+			speed -= friction;
+		}
+	}
+
+	// this occurs when a swipe is recognized
+	void updateDirection(){
+		Quaternion targetRotation = Quaternion.identity;
+
+		if (swipeRecognizer.completedSwipeDirection == TKSwipeDirection.Left){
+			targetRotation = (Quaternion.AngleAxis(90f, Vector3.up) * 2) * this.transform.rotation;
+		}
+		else if (swipeRecognizer.completedSwipeDirection == TKSwipeDirection.Right){
+			targetRotation = Quaternion.AngleAxis(90f, Vector3.down) * this.transform.rotation;
+		}
+		else if (swipeRecognizer.completedSwipeDirection == TKSwipeDirection.Up){
+			targetRotation = Quaternion.AngleAxis(90f, Vector3.right) * this.transform.rotation;
+		}
+		else if (swipeRecognizer.completedSwipeDirection == TKSwipeDirection.Down){
+			targetRotation = Quaternion.AngleAxis(90f, Vector3.left) * this.transform.rotation;
+		}
+		else if (swipeRecognizer.completedSwipeDirection == TKSwipeDirection.UpLeft){
+			targetRotation = Quaternion.AngleAxis(90f, new Vector3(1, 1, 0)) * this.transform.rotation;
+		}
+		else if (swipeRecognizer.completedSwipeDirection == TKSwipeDirection.UpRight){
+			targetRotation = Quaternion.AngleAxis(90f, new Vector3(1, -1, 0)) * this.transform.rotation;
+		}
+		else if (swipeRecognizer.completedSwipeDirection == TKSwipeDirection.DownLeft){
+			targetRotation = Quaternion.AngleAxis(90f, new Vector3(-1, 1, 0)) * this.transform.rotation;
+		}
+		else if (swipeRecognizer.completedSwipeDirection == TKSwipeDirection.DownRight){
+			targetRotation = Quaternion.AngleAxis(90f, new Vector3(-1, -1, 0)) * this.transform.rotation;
+		}
+
+		Debug.Log("swipe speed:  " + swipeRecognizer.swipeVelocity);
+		Debug.Log(swipeRecognizer.completedSwipeDirection);
+		speed = swipeRecognizer.swipeVelocity * speedScale;
+
+		var degrees = Quaternion.Angle(this.transform.rotation, targetRotation);
+
+		StartCoroutine(rotateOverTime(this.transform.rotation, targetRotation, 0.25f));
+
+		swipeRecognizer.resetSwipeDirection();
+
 	}
 
 	/*
@@ -47,17 +85,22 @@ public class Cube_rotation : MonoBehaviour{
 		a good guide on (s)lerp
 		https://forum.unity.com/threads/smooth-slerp-rotation.458468/
 	 */
-	IEnumerator rotateOverTime(Quaternion origin,Quaternion target,float timeDuration){
+	IEnumerator rotateOverTime(Quaternion origin, Quaternion target, float timeDuration){
 		if (timeDuration > 0f){
 			float startTime = Time.time;
 			float endTime = startTime + timeDuration;
+
+			this.transform.rotation = origin;
+
 			yield return null;
+
 			while(Time.time < endTime){
-				float progress = (Time.time - startTime)/timeDuration;
-				this.transform.rotation = Quaternion.Slerp(origin,target,progress);
+				float progress = (Time.time - startTime) / timeDuration;
+				this.transform.rotation = Quaternion.Slerp(origin, target, progress);
 				yield return null;
 			}
 		}
+
 		this.transform.rotation = target;
 	}
 }
